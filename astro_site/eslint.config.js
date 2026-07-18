cfa27d28f9a89fa97323d5d95abbe256d353a537
Rain <muther22@gmail.com>
2026-07-17 20:13:52 -0400

    Added type checking for verbs/index.astro

--- astro_site/eslint.config.js
+++ astro_site/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import astroPlugin from "eslint-plugin-astro";
 import astroParser from "astro-eslint-parser";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -41,6 +42,15 @@ export default [
         parser: tsParser,
         extraFileExtensions: [".astro"],
       },
+    },
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.astro"],
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
     },
   },
 
